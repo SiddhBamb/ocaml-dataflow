8ddaa03b3a28238fd267325bda844021bb0a19dd
Alex Chen <alexchen3141@gmail.com>
2025-04-14 11:06:41 -0500

    removed Core dependency, fixed bugs, ran mapreduce with word count example

--- lib/dataflow.ml
+++ lib/dataflow.ml
@@ -1,21 +1,13 @@
-open Core
-
-(* generic comparable type*)
-module type ORDERED_TYPE = sig
-    type t
-    val compare : t -> t -> int
-end
-
 (* like template in c++, we can make mapreduce for any key type *)
-module MakeMapReduce (Key : ORDERED_TYPE) = struct
-    module KMap = Map.Make(Key) (* Use the provided Key *)
+module MakeMapReduce (Key : Map.OrderedType) = struct
+    module KMap = Map.Make(Key)
 
     type 'a input_chunk = 'a list
 
     (* map: takes list of input chunks, makes it into a map for each chunk *)
     (* in word count example: takes list of lines, makes map from word to count *)
     let map_task_runner (input_chunks: 'a input_chunk list) (map_func: 'a input_chunk -> 'v KMap.t) : 'v KMap.t list =
-        List.map input_chunks ~f:map_func
+        List.map map_func input_chunks
     ;;
 
     (* takes all the different maps from each map task, merges into single map from key 
@@ -23,28 +15,30 @@ module MakeMapReduce (Key : ORDERED_TYPE) = struct
     (* in word count example: takes map from word to count for each line, merges into single map from word to list of counts *)
     let shuffle_task_runner (maps: 'v KMap.t list) : 'v list KMap.t =
         let init_map : 'v list KMap.t = KMap.empty in
-        List.fold_left maps ~init:init_map ~f:(fun acc_map current_map ->
-            KMap.merge acc_map current_map ~f:(fun ~key:_ merge_result ->
-                match merge_result with
-                | `Left list_val -> Some list_val (* Key only in acc, keep list *)
-                | `Right v_val -> Some [v_val]   (* Key only in current, create list *)
-                | `Both (list_val, v_val) -> Some (v_val :: list_val) (* Key in both, prepend *)
-            )
-        )
+        let merge_fun (_key : Key.t) (list_opt : 'v list option) (v_opt : 'v option) : 'v list option =
+            match list_opt, v_opt with
+            | Some list_val, None -> Some list_val (* Key only in acc, keep list *)
+            | None, Some v_val -> Some [v_val]   (* Key only in current, create list *)
+            | Some list_val, Some v_val -> Some (v_val :: list_val) (* Key in both, prepend *)
+            | None, None -> None (* Should not happen in Map.merge logic, but handle defensively *)
+        in
+        List.fold_left (fun acc_map current_map ->
+            KMap.merge merge_fun acc_map current_map
+        ) init_map maps
     ;;
 
     (* reduce: takes map from key to list of values w/ that key, reduces to 
      map from key to single value *)
     (* in word count example: takes map from word to list of counts, reduces to 
      map from word to total count *)
-    let reduce_task_runner (map: 'v list KMap.t) (reduce_func: key:Key.t -> data:'v list -> 'r) : 'r KMap.t =
-        KMap.mapi map ~f:reduce_func
+    let reduce_task_runner (map: 'v list KMap.t) (reduce_func: Key.t -> 'v list -> 'r) : 'r KMap.t =
+        KMap.mapi reduce_func map
     ;;
 
     (* runs the whole pipeline *)
     let run (input_chunks: 'a input_chunk list)
             ~(map_func: 'a input_chunk -> 'v KMap.t)
-            ~(reduce_func: key:Key.t -> data:'v list -> 'r) : 'r KMap.t =
+            ~(reduce_func: Key.t -> 'v list -> 'r) : 'r KMap.t =
         let map_outputs = map_task_runner input_chunks map_func in
         let shuffled_map = shuffle_task_runner map_outputs in
         reduce_task_runner shuffled_map reduce_func
